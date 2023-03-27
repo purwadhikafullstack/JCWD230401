376dd53be8fb45ec2fcd1016c2205ef481b041cb
marxtinn <butarbutar.m@gmail.com>
2023-03-27 20:55:13 +0700

    change register page picture

--- projects/client/src/Pages/UserRegister.jsx
+++ projects/client/src/Pages/UserRegister.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
     Button,
     Checkbox,
@@ -10,27 +10,35 @@ import {
     Link,
     Stack,
     Image,
-    Text, Icon, HStack, Box, Divider, Center, Card, CardBody, InputGroup, InputRightElement, useDisclosure
-} from '@chakra-ui/react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { API_URL } from '../helper';
-
+    Text,
+    Icon,
+    HStack,
+    Box,
+    Divider,
+    Center,
+    Card,
+    CardBody,
+    InputGroup,
+    InputRightElement,
+    useDisclosure,
+} from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../helper";
 
 //user
 export default function UserRegister() {
     const [showPassword, setShowPassword] = useState(false);
-    const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
+    const [showPasswordConfirmation, setShowPasswordConfirmation] =
+        useState(false);
     const navigate = useNavigate();
-    const [name, setName] = React.useState('');
-    const [phone, setPhone] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [passwordConfirmation, setPasswordConfirmation] = React.useState('');
+    const [name, setName] = React.useState("");
+    const [phone, setPhone] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = React.useState("");
     const { isOpen, onOpen, onClose } = useDisclosure();
-
-
 
     const onBtnRegister = async () => {
         try {
@@ -39,45 +47,51 @@ export default function UserRegister() {
                 phone: phone,
                 email: email,
                 password: password,
-                confirmationPassword: passwordConfirmation
-            }
-            );
+                confirmationPassword: passwordConfirmation,
+            });
             console.log("ini hasil response onbtnregister :", response); //testing purposes
-            console.log("ini hasil response onbtnregister message from be :", response.data.message); //testing purposes
+            console.log(
+                "ini hasil response onbtnregister message from be :",
+                response.data.message
+            ); //testing purposes
             if (response.data.success) {
                 alert(response.data.message);
             }
-
         } catch (error) {
             console.log("ini error dari onBtnRegister : ", error); //testing purposes
             alert(error.response.data.message);
             alert(error.response.data.error[0].msg);
             alert(error.response.data.error[1].msg);
         }
-    }
+    };
 
     return (
-        <Stack minH={{ lg: '100vh' }}
-            direction={{ base: 'column', md: 'column', lg: 'row' }}
+        <Stack
+            minH={{ lg: "100vh" }}
+            direction={{ base: "column", md: "column", lg: "row" }}
         >
             <Flex
-                p={{ base: '8', sm: '0' }}
+                p={{ base: "8", sm: "0" }}
                 flex={1}
-                align={'center'} justify={'center'}
-            // py='20'
+                align={"center"}
+                justify={"center"}
+                // py='20'
             >
-                <Stack spacing={0} w={'full'} maxW={{ base: 'sm' }}>
-                    <Heading fontSize={'3xl'} fontWeight='semibold'
-                        my='8'
-                    >Register to tempatku</Heading>
+                <Stack spacing={0} w={"full"} maxW={{ base: "sm" }}>
+                    <Heading fontSize={"3xl"} fontWeight="semibold" my="8">
+                        Register to tempatku
+                    </Heading>
                     <Stack spacing={2}>
                         <HStack>
                             <Box>
                                 {/* NAME */}
                                 <FormControl id="Name">
                                     <FormLabel>Name</FormLabel>
-                                    <Input type="text"
-                                        onChange={(e) => setName(e.target.value)}
+                                    <Input
+                                        type="text"
+                                        onChange={(e) =>
+                                            setName(e.target.value)
+                                        }
                                     />
                                 </FormControl>
                             </Box>
@@ -85,8 +99,11 @@ export default function UserRegister() {
                                 {/* PHONE */}
                                 <FormControl id="phonenumber">
                                     <FormLabel>Phone number</FormLabel>
-                                    <Input type="text"
-                                        onChange={(e) => setPhone(e.target.value)}
+                                    <Input
+                                        type="text"
+                                        onChange={(e) =>
+                                            setPhone(e.target.value)
+                                        }
                                     />
                                 </FormControl>
                             </Box>
@@ -94,7 +111,8 @@ export default function UserRegister() {
                         {/* EMAIL */}
                         <FormControl id="email">
                             <FormLabel>Email address</FormLabel>
-                            <Input type="email"
+                            <Input
+                                type="email"
                                 onChange={(e) => setEmail(e.target.value)}
                             />
                         </FormControl>
@@ -104,18 +122,27 @@ export default function UserRegister() {
                             <InputGroup>
                                 {/* Input Password */}
                                 <Input
-                                    type={showPassword ? 'text' : 'password'}
-                                    onChange={(e) => setPassword(e.target.value)}
+                                    type={showPassword ? "text" : "password"}
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
                                 />
-                                <InputRightElement h={'full'}>
+                                <InputRightElement h={"full"}>
                                     <Button
-                                        variant={'ghost'}
-                                        _hover={'none'}
-                                        _active={'none'}
+                                        variant={"ghost"}
+                                        _hover={"none"}
+                                        _active={"none"}
                                         onClick={() =>
-                                            setShowPassword((showPassword) => !showPassword)
-                                        }>
-                                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                                            setShowPassword(
+                                                (showPassword) => !showPassword
+                                            )
+                                        }
+                                    >
+                                        {showPassword ? (
+                                            <ViewIcon />
+                                        ) : (
+                                            <ViewOffIcon />
+                                        )}
                                     </Button>
                                 </InputRightElement>
                             </InputGroup>
@@ -125,29 +152,45 @@ export default function UserRegister() {
                             <InputGroup>
                                 {/* Input Password Confirmation*/}
                                 <Input
-                                    type={showPasswordConfirmation ? 'text' : 'password'} onChange={(e) => setPasswordConfirmation(e.target.value)}
+                                    type={
+                                        showPasswordConfirmation
+                                            ? "text"
+                                            : "password"
+                                    }
+                                    onChange={(e) =>
+                                        setPasswordConfirmation(e.target.value)
+                                    }
                                 />
-                                <InputRightElement h={'full'}>
+                                <InputRightElement h={"full"}>
                                     <Button
-                                        variant={'ghost'}
-                                        _hover={'none'}
-                                        _active={'none'}
+                                        variant={"ghost"}
+                                        _hover={"none"}
+                                        _active={"none"}
                                         onClick={() =>
-                                            setShowPasswordConfirmation((showPasswordConfirmation) => !showPasswordConfirmation)
-                                        }>
-                                        {showPasswordConfirmation ? <ViewIcon /> : <ViewOffIcon />}
+                                            setShowPasswordConfirmation(
+                                                (showPasswordConfirmation) =>
+                                                    !showPasswordConfirmation
+                                            )
+                                        }
+                                    >
+                                        {showPasswordConfirmation ? (
+                                            <ViewIcon />
+                                        ) : (
+                                            <ViewOffIcon />
+                                        )}
                                     </Button>
                                 </InputRightElement>
                             </InputGroup>
                         </FormControl>
                     </Stack>
                     {/* Khusus untuk daftar menjadi tenant */}
-                    <Stack
-                        pt='8'
-                    >
-                        <Button bg={'#D3212D'} color={'white'} variant={'solid'}
+                    <Stack pt="8">
+                        <Button
+                            bg={"#D3212D"}
+                            color={"white"}
+                            variant={"solid"}
                             _hover={{
-                                bg: '#D3212D',
+                                bg: "#D3212D",
                             }}
                             onClick={onBtnRegister}
                         >
@@ -155,22 +198,20 @@ export default function UserRegister() {
                         </Button>
                         {/* Login */}
                     </Stack>
-                    <Stack
-                        pb='6'
-                        px='10'
-                    >
-                        <Card variant='none' borderColor='#d0d7de'>
+                    <Stack pb="6" px="10">
+                        <Card variant="none" borderColor="#d0d7de" mt="2">
                             <CardBody>
                                 <Center>
-                                    <HStack fontSize='sm'
-                                    >
+                                    <HStack fontSize="sm">
                                         <Text>Have an account?</Text>
                                         {/* usenavigate ke landing, tp login nya ada di landing page modal, hrs bikin modal login lsg kebuka in order to have this  */}
-                                        <Text onClick={() => {
-                                            navigate('/');
-                                            // onOpen();
-                                        }} color='#0969da'
-                                            cursor={'pointer'}
+                                        <Text
+                                            onClick={() => {
+                                                navigate("/");
+                                                // onOpen();
+                                            }}
+                                            color="#0969da"
+                                            cursor={"pointer"}
                                         >
                                             Login.
                                         </Text>
@@ -181,17 +222,13 @@ export default function UserRegister() {
                     </Stack>
                 </Stack>
             </Flex>
-            <Flex
-                flex={1}
-                display={{ base: 'flex', sm: 'flex' }}
-            >
+            <Flex flex={1} display={{ base: "flex", sm: "flex" }}>
                 <Image
-                    alt={'User Register Page Image'}
-                    objectFit={'cover'}
+                    alt={"User Register Page Image"}
+                    objectFit={"cover"}
                     src={
-                        'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+                        "https://images.unsplash.com/photo-1597634285596-01fd1bffe116?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
                     }
-
                 />
             </Flex>
         </Stack>
